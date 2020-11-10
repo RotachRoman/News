1ac4fbe713b984db49aabf83811c79ea665692a1
RotachRoman <cuck.5@mail.ru>
2020-11-10 13:06:26 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Rotach Roman on 10.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
